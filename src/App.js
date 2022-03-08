56c123fc42680a13f9f5600dc78665357c91108f
bia-pequeno <bia.2017pequeno@gmail.com>
2022-03-08 17:29:56 -0300

    concluido

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
 import React, {Component} from 'react'
-import './App.css'
+import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+
+
+const Globalstyle = createGlobalStyle`
+*{
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+}
+`
+const Body = styled.div`
+   background-color: darkcyan;
+`
+const Container = styled.div`
+   height: 100vh;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: space-evenly;
+   color: white;
+`
+const Num = styled.h2`
+   font-size: 2vw;
+`
+
+const Count = styled.div`
+   height: 50vh;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: space-evenly;
+`
+const Button = styled.button`
+   background-color:#FFB10A;
+   border: none;
+   font-size: 4vw;
+   padding: 1vh 3vw;
+   color:blueviolet
+`
+
+
+
 
 export default class Counter extends Component {
   
@@ -30,14 +72,18 @@ export default class Counter extends Component {
     const {count} = this.state
     const {add,remove} = this
     return(
-      <div className='container'>
+      <Body>
+        <Container>
+        <Globalstyle/>
         <h1>React Counter App</h1>
-        <div className='counter'>
-        <button onClick={add}> + </button>
-        <h2>{count}</h2>
-        <button onClick={remove}> - </button>
-        </div>
-      </div>
+        <Count>
+        <Button onClick={add}> + </Button>
+        <Num>{count}</Num>
+        <Button onClick={remove}> - </Button>
+        </Count>
+      </Container>
+      </Body>
+      
     )
   }
 
